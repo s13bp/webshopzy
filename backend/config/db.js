82d58969d39566693d1604e0a0df699f7db4007f
s13bp <sahibbharajparminder@gmail.com>
2023-06-26 13:14:35 +0300

    jh

---
+++ backend/config/db.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect('mongodb+srv://sahib:sahib@sahibcluster.blamrdp.mongodb.net/megameta', {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useCreateIndex: true,
+    })
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline)
+  } catch (error) {
+    console.error(`Error: ${error.message}`.red.underline.bold)
+    process.exit(1)
+  }
+}
+
+export default connectDB
---
+++ backend/data/products.js
@@ -0,0 +1,30 @@
+const products = [
+  {
+    name: 'Ledger Nano X',
+    image: '/images/ledger.jpeg',
+    description:
+      'Be safe with your crpto',
+    brand: 'Satoshi',
+    category: 'Electronics',
+    price: 180.99,
+    countInStock: 13,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'MegaMeta Ebook',
+    image: '/images/Unknown.jpeg',
+    description:
+      'Set yourself free financially my dude',
+    brand: 'MacMillan',
+    category: 'Books',
+    price: 0.09,
+    countInStock: 10,
+    rating: 0,
+    numReviews: 0,
+  },
+
+
+]
+
+export default products
